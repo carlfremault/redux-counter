1417d9745abff0c157151f23dfc12e0cdb380464
Carl Fremault <carlfremault@yahoo.com>
2023-02-02 20:05:36 +0100

    first commit

---
+++ src/components/Counter.js
@@ -0,0 +1,33 @@
+import classes from './Counter.module.css';
+import { useSelector, useDispatch } from 'react-redux';
+
+const Counter = () => {
+  // useDispatch gives back a dispatch function
+  const dispatch = useDispatch();
+  // useSelector will be executed by redux
+  // react-redux automatically sets up subscription
+  const counter = useSelector((state) => state.counter);
+
+  const incrementHandler = () => {
+    dispatch({ type: 'increment' });
+  };
+  const decrementHandler = () => {
+    dispatch({ type: 'decrement' });
+  };
+
+  const toggleCounterHandler = () => {};
+
+  return (
+    <main className={classes.counter}>
+      <h1>Redux Counter</h1>
+      <div className={classes.value}>{counter}</div>
+      <div>
+        <button onClick={incrementHandler}>Increment</button>
+        <button onClick={decrementHandler}>Decrement</button>
+      </div>
+      <button onClick={toggleCounterHandler}>Toggle Counter</button>
+    </main>
+  );
+};
+
+export default Counter;
